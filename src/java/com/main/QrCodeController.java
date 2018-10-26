173300ff7ceafa36c27b383dad69682aba21934a
Thong Do <dovanthong97@gmail.com>
2018-10-26 21:14:34 +0700

    second

---
+++ src/java/com/main/QrCodeController.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.main;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigInteger;
+import java.util.Random;
+import javax.imageio.ImageIO;
+import sun.misc.BASE64Decoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author DoThong
+ */
+public class QrCodeController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try {
+            StringBuffer buffer = new StringBuffer();
+            Reader reader = request.getReader();
+            int current;
+            while ((current = reader.read()) >= 0) {
+                buffer.append((char) current);
+            }
+            String data = new String(buffer);
+            data = data.substring(data.indexOf(",") + 1);
+            Result result = null;
+            BufferedImage image = null;
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] imgBytes = decoder.decodeBuffer(data);    
+            InputStream in = new ByteArrayInputStream(imgBytes);
+            image = ImageIO.read(in);
+            LuminanceSource source = new BufferedImageLuminanceSource(image);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try {
+                result = new MultiFormatReader().decode(bitmap);
+            } catch (NotFoundException e) {
+                // fall thru, it means there is no QR code in image
+            }
+
+            if (result != null) {
+                System.out.println("QR code data is: " + result.getText());
+            }
+//            System.out.println("PNG image data on Base64: " + data);
+//            FileOutputStream output = new FileOutputStream(
+//                    new File("C:\\Users\\DoThong\\Desktop\\"
+//                            + new Random().nextInt(100000) + ".png"));
+//            output.write(new BASE64Decoder().decodeBuffer(data));
+//            output.flush();
+//            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
